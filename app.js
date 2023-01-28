a6f086166720e082b5941cf5882b3cd3157d34d3
Sergen Türkmendağ <sergenturkmendag@gmail.com>
2023-01-28 23:37:39 +0200

    lesson 2

--- app.js
+++ app.js
@@ -3,8 +3,22 @@ import express from "express";
 const app = express()
 const port = 3000
 
+// ejs template engine
+
+app.set('view engine', 'ejs');
+
+
+
+
+// statik dosyalarımız
+app.use(express.static('public'));
+
+
 app.get("/", (req, res)=>{
-    res.send("İndex sayfası 2")
+    res.render("index")
+})
+app.get("/about", (req, res)=>{
+    res.render("about")
 })
 
 app.listen(port, ()=>{
