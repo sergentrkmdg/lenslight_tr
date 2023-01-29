c2de6d1187b8d773028c98ad0dce52818af70415
Sergen Türkmendağ <sergenturkmendag@gmail.com>
2023-01-30 01:14:42 +0200

    router işlemleri

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import conn from "./db.js";
+import pageRoute from "./routes/pageRoute.js";
 
 dotenv.config();
 
@@ -20,13 +21,16 @@ app.set('view engine', 'ejs');
 // statik dosyalarımız
 app.use(express.static('public'));
 
+//routes
+app.use("/",pageRoute);
 
-app.get("/", (req, res)=>{
+//routes işlemleri diğer yöntem
+/* app.get("/", (req, res)=>{
     res.render("index")
 })
 app.get("/about", (req, res)=>{
     res.render("about")
-})
+}) */
 
 app.listen(port, ()=>{
     console.log(`Uygulama bu portta başlatilacak port:${port}`);
