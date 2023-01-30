7f7fa9b5abcef5774a09e0024cec5cb8f7447fc7
Sergen Türkmendağ <sergenturkmendag@gmail.com>
2023-01-30 14:14:18 +0200

    mongoose ile şema oluşturma

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import conn from "./db.js";
 import pageRoute from "./routes/pageRoute.js";
+import photoRoute from "./routes/photoRoute.js";
+
 
 dotenv.config();
 
@@ -23,6 +25,7 @@ app.use(express.static('public'));
 
 //routes
 app.use("/",pageRoute);
+app.use("/photos", photoRoute);
 
 //routes işlemleri diğer yöntem
 /* app.get("/", (req, res)=>{
