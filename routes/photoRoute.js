ac12cb8b884ef5a83bb27841e7035f8519461c87
Sergen Türkmendağ <sergenturkmendag@gmail.com>
2023-01-31 15:19:08 +0200

    Url parametre kullanimi

--- routes/photoRoute.js
+++ routes/photoRoute.js
@@ -6,6 +6,7 @@ const router = express.Router();
 
 router.route("/").post(photoController.createPhoto);
 router.route("/").get(photoController.getAllPhotos);
+router.route("/:id").get(photoController.getAPhoto);
 
 
 export default router;
