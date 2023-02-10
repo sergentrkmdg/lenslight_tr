3dfd47d8afc7b0954162e5a6d91ed92ebb09e6f0
Sergen Türkmendağ <sergenturkmendag@gmail.com>
2023-02-11 01:11:41 +0200

    Cloudinary ile görsel depolama

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -1,15 +1,26 @@
 import Photo from "../models/photoModel.js";
+import { v2 as cloudinary} from "cloudinary";
+import fs from "fs";
 
 const createPhoto = async(req, res)=>{
-
+    const result = await cloudinary.uploader.upload(
+        req.files.image.tempFilePath,  // urlde ki resim için geçici temp dosyası tutar
+        {
+          use_filename: true,
+          folder: 'lenslight_tr',
+        }
+      );
+    
     try { 
     await Photo.create({
         name:req.body.name,
         description:req.body.description,
         user:res.locals.user._id,
+        url:result.secure_url,
 
     });
-
+     
+    fs.linkSync(req.files.image.tempFilePath);
     res.status(201).redirect("/users/dashboard"); 
     } catch (error) {
         res.status(500).json({
@@ -35,7 +46,7 @@ const getAllPhotos = async (req, res) =>{
 };
 const getAPhoto = async (req, res) =>{
     try {
-        const photo = await Photo.findById({_id:req.params.id})
+        const photo = await Photo.findById({_id:req.params.id}).populate("user");
         res.status(200).render("photo", {
             photo,
             link:"photos",
