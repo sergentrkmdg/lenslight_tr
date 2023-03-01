138b8ca7a41a8f9451286fc999327d8454d7c445
Sergen Türkmendağ <sergenturkmendag@gmail.com>
2023-03-02 01:15:47 +0200

    nodemailer ile iletisim sayfası

--- routes/pageRoute.js
+++ routes/pageRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-import {getIndexPage, getAboutPage, getRegisterPage, getLoginPage, getLogout} from "../controllers/pageController.js"
+import {getIndexPage, getAboutPage, getRegisterPage, getLoginPage, getLogout, getContactPage, sendMail} from "../controllers/pageController.js"
 
 const router = express.Router();
 
@@ -8,5 +8,7 @@ router.route("/about").get(getAboutPage);
 router.route("/register").get(getRegisterPage);
 router.route("/login").get(getLoginPage);
 router.route("/logout").get(getLogout);
+router.route("/contact").get(getContactPage);
+router.route("/contact").post(sendMail);
 
 export default router;
